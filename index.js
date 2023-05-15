cfea776ef1576ea1c871bfc76a8741812bff933f
wonde21 <wondim878@gmail.com>
2023-05-15 05:40:10 +0300

    some functional

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-const userChoice = document.querySelector("#player").value;
-
-
-document.getElementsByTagName("form").onsubmit = function(){
-    document.querySelector(".result h2").innerHTML = "usd";
+// const userChoice = document.querySelector("#player").value;
+class Data{
+    constructor(item){
+        this.item = item;
+    }
+    index = function(n){
+        const posibility =["rock", "scissors","paper"];
+        return posibility.indexOf(`${n}`)
+       }
     
 }
+const btn = document.querySelector("button");
 
-function computer(){
-    let playerChoice =["Rock","paper", "scissors"];
-    let randIndex = Math.floor(Math.random()*3);
+const msg = document.querySelector(".result h2");
+const posibility =["rock", "scissors","paper"];
+var playerIndex ;
+// var playerChoice;
+// console.log(playerChoice + " upper");
 
-    return playerChoice[randIndex];
+btn.addEventListener('click',function(){
+    const playerChoice = document.querySelector("#lucky").value.toLowerCase();
+    
+    playerIndex = posibility.indexOf(`${playerChoice}`);
+        
+    
+var computer_random = Math.floor(Math.random()*3);
+// var playerIndex =posibility.indexOf(playerChoice);
 
-}
-
+if(computer_random+1 == playerIndex || computer_random == playerIndex+1 ||(computer_random == 0 && playerIndex == 2) || (computer_random == 2 && playerIndex == 0)){
+        if(computer_random + 1 == playerIndex){
+            msg.innerText="you win!";
+            btn.style.backgroundColor = "green";
+        }
+        else if(computer_random == playerIndex + 1){
+            msg.innerText="you lose!";
+            btn.style.backgroundColor = "red";
+        }else if (computer_random + 2 == playerIndex){
+            msg.innerText = "you lose!";
+            btn.style.backgroundColor = "red";
+        }else if(computer_random ==playerIndex + 2){
+            msg.innerText = "you win!";
+            btn.style.backgroundColor = "green";
+        }
+        
+    }else if(playerIndex === -1){
+    msg.innerText = "INCORRECT CHOICE please Try Again!" + playerChoice;
+    }else{
+    msg.innerText ="Try Again!" +playerChoice +playerIndex + computer_random;
+    }
+});
